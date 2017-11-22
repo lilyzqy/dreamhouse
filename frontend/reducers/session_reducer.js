a3ffcb1b26356051cb548a4ff074c3be2ec8f63c
Lily Yang <zq.yang.lily@gmail.com>
2017-11-22 10:37:54 -0800

    done session reducer and modified session action

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,25 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
+
+const _nullUser = {
+  currentUser: null
+};
+
+const sessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, { currentUser });
+    case LOGOUT_CURRENT_USER:
+      return _nullUser;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
