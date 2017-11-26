8d85da4bb03ba307b45f01c149223640f43b9a65
Lily Yang <zq.yang.lily@gmail.com>
2017-11-25 18:00:31 -0800

    edited pic reducer

--- frontend/reducers/picture_reducer.js
+++ frontend/reducers/picture_reducer.js
@@ -1,10 +1,12 @@
 import merge from 'lodash/merge';
 
-import { RECEIVE_PIC, REMOVE_PIC } from '../actions/picture_actions';
+import { RECEIVE_PICS, RECEIVE_PIC, REMOVE_PIC } from '../actions/picture_actions';
 
 const pictureReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_PICS:
+      return merge({},state, action.pictures);
     case RECEIVE_PIC:
       return merge({},state,{[action.picture.id]:action.picture});
     case REMOVE_PIC:
